568c8e9f768802d13dcbd48178290f48722690fc
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-12-24 11:15:43 +0100

    Update font settings and add CSS imports
    Comment out the Oswald font setting and add CSS imports.

--- scripts/dre_top5.R
+++ scripts/dre_top5.R
@@ -58,9 +58,10 @@ df %>%
   gt_theme_savant() %>%
   tab_options(
     heading.align = "left",
-    table.font.names = "Oswald",
+    #table.font.names = "Oswald",
     table.additional_css = ".gt_table {
                 margin-bottom: 40px;
+  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&display=swap');
   @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/brands.min.css');
               }"
   ) %>%
